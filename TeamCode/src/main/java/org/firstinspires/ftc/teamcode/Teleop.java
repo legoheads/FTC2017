468b6103159da491c7c73c16735fce2392eeae18
rohandangsharma <rohandangsharma@gmail.com>
2017-11-11 20:34:52 -0500

    Slower teleOp Turning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -88,17 +88,16 @@ public class Teleop extends LinearOpMode {
         //LOOP BELOW
         //While the op mode is active, do anything within the loop
         //Note we use opModeIsActive() as our loop condition because it is an interruptible method.
-        while (opModeIsActive())
-        {
+        while (opModeIsActive()) {
             //Set float variables as the inputs from the joysticks and the triggers
             drivePowerFast = gamepad1.left_stick_y;
             shiftPowerFast = gamepad1.left_stick_x;
             drivePowerSlow = gamepad2.left_stick_y / 3;
             shiftPowerSlow = gamepad2.left_stick_x / 3;
-            fastLeftTurnPower = (float) (gamepad1.left_trigger / 1.5);
-            fastRightTurnPower = (float) (gamepad1.right_trigger / 1.5);
-            slowLeftTurnPower = gamepad2.left_trigger / 2;
-            slowRightTurnPower = gamepad2.right_trigger / 2;
+            fastLeftTurnPower = (float) (gamepad1.left_trigger / 2.5);
+            fastRightTurnPower = (float) (gamepad1.right_trigger / 2.5);
+            slowLeftTurnPower = gamepad2.left_trigger / 3;
+            slowRightTurnPower = gamepad2.right_trigger / 3;
             liftPower = -gamepad2.right_stick_y;
 
 
